9bfb5183fd8d5c6eb3e638b449f398e05c444380
Minsang <minsang1233@gmail.com>
2025-12-05 13:05:18 +0900

    chore: spring boot init-setting

---
+++ src/test/java/com/Minyou/MINYOU/MinyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Minyou.MINYOU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
