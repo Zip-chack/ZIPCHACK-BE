1c54bee395fb7b0c6a5329041f3a93380d293ba3
jiucchu <jiucchu@gmail.com>
2025-12-05 14:29:39 +0900

    feat: logging interceptor

---
+++ src/main/java/com/Minyou/MINYOU/interceptor/LoggingInterceptor.java
@@ -0,0 +1,24 @@
+package com.Minyou.MINYOU.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info("Request: {} {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        log.info("Response: {} - Status: {}", request.getRequestURI(), response.getStatus());
+    }
+}
+
