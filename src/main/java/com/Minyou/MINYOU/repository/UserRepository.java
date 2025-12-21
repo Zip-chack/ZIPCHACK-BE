fbc19e36d996b908767d8c3ee3aac79246fcbddc
jiucchu <jiucchu@gmail.com>
2025-12-21 18:10:15 +0900

    fix: dto 변경에 따른 레포지토리 수정

--- src/main/java/com/Minyou/MINYOU/repository/UserRepository.java
+++ src/main/java/com/Minyou/MINYOU/repository/UserRepository.java
@@ -10,5 +10,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    Optional<User> findByResetToken(String resetToken);
 }
 
